92fe235bcf6a2b8b4172b280d9f9b03e684ac026
Hrishikesh Nene <hrishi@mycariq.com>
2022-02-20 22:58:12 +0530

    Added manifest

--- src/shabdak2/Sthiranka.java
+++ src/shabdak2/Sthiranka.java
@@ -8,13 +8,53 @@ public class Sthiranka {
 	public static final char[] Moolakshare = { 'अ', 'आ', 'इ', 'ई', 'उ', 'ऊ', 'ए', 'ऐ', 'ओ', 'औ', 'क', 'ख', 'ग', 'घ',
 			'ङ', 'च', 'छ', 'ज', 'झ', 'ञ', 'ट', 'ठ', 'ड', 'ढ', 'ण', 'त', 'थ', 'द', 'ध', 'न', 'प', 'फ', 'ब', 'भ', 'म',
 			'य', 'र', 'ल', 'व', 'श', 'ष', 'स', 'ह', 'ळ', 'क़', 'ख़', 'ग़', 'ज़', 'ड़', 'ढ़', 'फ़', 'य़' };
-	
-	public static final int[] Swaransh = { 0x0902, 0x093e, 0x093f, 0x0940, 0x0941, 0x0942, 0x0943, 0x0945, 0x0946, 0x0947,
-			0x0948, 0x0949, 0x094a, 0x094b, 0x094c, 0x094d, 0x094f, 0x0971};
+
+	public static final int[] Swaransh = { 0x0902, 0x093e, 0x093f, 0x0940, 0x0941, 0x0942, 0x0943, 0x0945, 0x0946,
+			0x0947, 0x0948, 0x0949, 0x094a, 0x094b, 0x094c, 0x094d, 0x094f, 0x0971 };
 
 	// Small subset of 3 and 4 letters
-	public static final String[] WordList = { "समूळ", "जठर", "कुलूप", "पंचम", "निषेध", "सुखरूप", "संकेत", "रंगीत",
-			"कुटुंब", "तांडव", "माधुकरी", "गंमत" };
+	public static final String[] WordList = { 
+			"समूळ", 
+			"जठर",
+			"कुलूप", 
+			"पंचम",
+			"निषेध", 
+			"सुखरूप", 
+			"संकेत", 
+			"रंगीत",
+			"कुटुंब", 
+			"तांडव", 
+			"माधुकरी",
+			"पाहूणा",
+			"खादाड",
+			"अपार",
+			"निवास",
+			"उतावीळ",
+			"विभाग",
+			"उपवास",
+			"दूषण",
+			"निराकार",
+			"मोकाट",
+			"परंपरा",
+			"संकट",
+			"अडाणी",
+			"मोकाट",
+			"कबुली",
+			"संमती",
+			"करूणा",
+			"अनुभूती",
+			"दुजोरा",
+			"पाठिंबा",
+			"संयोग",
+			"आधार",
+			"संबंध",
+			"चौकशी",
+			"मानहानी",
+			"विकलांग",
+			"संशोधन",
+			"खटला",
+			"आरोप",
+			"गंमत" };
 
 	public static final String RANDOM = "Random";
 
@@ -37,7 +77,7 @@ public class Sthiranka {
 		public static final String ANSI_PURPLE = "\u001B[35m";
 		public static final String ANSI_CYAN = "\u001B[36m";
 		public static final String ANSI_WHITE = "\u001B[37m";
-		
+
 		public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
 		public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
 		public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
@@ -45,6 +85,6 @@ public class Sthiranka {
 		public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
 		public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 		public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-		public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";		
+		public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	}
 }
